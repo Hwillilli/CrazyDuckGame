ae51833742ef2a45908659511f4a7eba4e361660
Hwil lilli <sughee3@naver.com>
2023-01-10 12:42:40 +0900

    에셋
    나머지

---
+++ Assets/FindPlayer.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+namespace Yarn.Unity.Example
+{
+    public class FindPlayer : YarnCharacterView
+    {
+        public GameObject YarnCharacterView;
+
+        void Start()
+        {
+            //if (YarnCharacterView.playerCharacter == null)
+            //{
+            //    YarnCharacterView.playerCharacter = GameObject.FindWithTag("Player");
+            //    allCharacters.Add();
+            //}
+        }
+
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ Assets/README.md
@@ -0,0 +1,2 @@
+# CrazyDuckGame
+ 미친오리게임 개발
