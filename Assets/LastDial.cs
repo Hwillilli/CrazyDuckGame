d75589bdf91855c1ed127d2a79478589a84d9de8
Hwil lilli <sughee3@naver.com>
2023-08-15 20:57:31 +0900

    마지막 연주
    마지막 연주 영상 추가~~

--- Assets/LastDial.cs
+++ Assets/LastDial.cs
@@ -10,19 +10,19 @@ public class LastDial : MonoBehaviour
 
     private void Update()
     {
-        for (int i = 0; i < dialog.Length; i++)
-        {
-            if (!dialog *.activeInHierarchy)
-            {
-                allActive = false;
-                break; //this ¡°quits¡± the for loop, you don¡¯t need to check the rest of the items if you already found one that¡¯s not active
+        //for (int i = 0; i < dialog.length; i++)
+        //{
+        //    if (!dialog *.activeinhierarchy)
+        //    {
+        //        allactive = false;
+        //        break; //this ¡°quits¡± the for loop, you don¡¯t need to check the rest of the items if you already found one that¡¯s not active
 
-                if (allActive)
-                {
-                    DoSomething();
-                }
+        //        if (allactive)
+        //        {
+        //            dosomething();
+        //        }
 
-            }
-        }
+        //    }
+        //}
     }
 }
