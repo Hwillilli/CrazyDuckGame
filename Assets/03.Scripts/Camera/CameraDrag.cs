d2db6626b8ca09fabf4a50510339368df4030bb9
Hwil lilli <sughee3@naver.com>
2023-10-03 00:43:20 +0900

    찐 마무리

--- Assets/03.Scripts/Camera/CameraDrag.cs
+++ Assets/03.Scripts/Camera/CameraDrag.cs
@@ -7,6 +7,7 @@ public class CameraDrag : MonoBehaviour
     private Vector3 touchStartPos;
     private Vector3 cameraStartPos;
     public GameObject image; // 이미지 오브젝트를 연결합니다.
+    public float smoothness = 0.1f;
 
     void Update()
     {
@@ -20,20 +21,18 @@ public class CameraDrag : MonoBehaviour
             Vector3 touchCurrentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3 offset = touchCurrentPos - touchStartPos;
 
-            // 드래그 영역을 이미지 크기로 제한합니다.
             float imageWidth = image.GetComponent<Renderer>().bounds.size.x;
             float cameraWidth = Camera.main.orthographicSize * 2 * Camera.main.aspect;
             float minX = image.transform.position.x - imageWidth / 2 + cameraWidth / 2;
             float maxX = image.transform.position.x + imageWidth / 2 - cameraWidth / 2;
 
-            // 카메라의 X 위치를 조절합니다.
             Vector3 newPosition = new Vector3(
                 Mathf.Clamp(cameraStartPos.x - offset.x, minX, maxX),
                 transform.position.y,
                 transform.position.z
             );
 
-            transform.position = newPosition;
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * smoothness);
         }
     }
 }
